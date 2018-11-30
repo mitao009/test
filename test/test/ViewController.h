334c3304b727840d2d98430745740b579f9475bb
Tony <>
2018-11-30 10:56:33 +0800

    no message

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Tony on 30/11/18.
+//  Copyright © 2018 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by Tony on 30/11/18.
+//  Copyright © 2018 Tony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
